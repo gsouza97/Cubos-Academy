69fb238a810180e656b4af48a52dc70549f86950
Gabriel Souza <gabrielss_@outlook.com>
2021-04-03 00:18:06 -0300

    basic layout

--- aula_03/lib/main.dart
+++ aula_03/lib/main.dart
@@ -8,12 +8,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text('Contador'),
-        centerTitle: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Contador'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Você pressionou o botão:',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+              ),
+              Text(
+                '0',
+                style: TextStyle(fontSize: 30),
+              ),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Icon(Icons.add),
+        ),
       ),
-      body: Container(),
-    ));
+    );
   }
 }
