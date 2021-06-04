042887ee3f291e83567db7ab2821bcade076f111
Gabriel Souza <gabrielss_@outlook.com>
2021-06-04 11:24:59 -0300

    showing picture

--- camera_app/README.md
+++ camera_app/README.md
@@ -4,4 +4,4 @@ A quick exercise to learn how to use Camera and how to take a picture.
 
 ## Screenshots
 
-<img src="screenshots/Screenshot_1622813701.png" width=230/> <img src="screenshots/Screenshot_1622813706.png" width=230/>
+<img src="screenshots/Screenshot_1622813701.png" width=230/> <img src="screenshots/Screenshot_1622813706.png" width=230/> <img src="screenshots/Screenshot_1622816359.png" width=230/>
--- camera_app/lib/picture_view.dart
+++ camera_app/lib/picture_view.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:camera/camera.dart';
 import 'package:camera_app/camera_view.dart';
 import 'package:flutter/material.dart';
@@ -10,36 +12,58 @@ class PictureView extends StatefulWidget {
 }
 
 class _PictureViewState extends State<PictureView> {
+  // Variável que recebe a foto do pop
+  XFile _picture;
+
   // Função para mostrar a camera.
   // Primeiramente tem que pegar as cameras que estão disponíveis no aparelho
   // Em seguida escolhe uma das cameras
   // Depois abre uma tela mostrando a camera.
+  // Pega a foto que foi tirada e salva em _picture. SetState pra exibir
   _showCamera() async {
     final cameras = await availableCameras();
     final camera = cameras.first;
 
-    final result = Navigator.of(context).push(
+    final result = await Navigator.of(context).push(
       MaterialPageRoute(
-        builder: (context) => CameraView(camera: camera,),
+        builder: (context) => CameraView(
+          camera: camera,
+        ),
       ),
     );
+    setState(() {
+      _picture = result;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Take a self clicking on the button below'),
-            ElevatedButton(
-              onPressed: () {
-                _showCamera();
-              },
-              child: Text('Open Camera'),
-            )
-          ],
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Take a self clicking on the button below'),
+              ElevatedButton(
+                onPressed: () {
+                  _showCamera();
+                },
+                child: Text('Open Camera'),
+              ),
+              SizedBox(height: 30),
+              _picture != null
+                  ? Container(
+                      width: double.infinity,
+                      height: 400,
+                      child: Image.file(
+                        File(_picture.path),
+                        fit: BoxFit.cover,
+                      ),
+                    )
+                  : Container(),
+            ],
+          ),
         ),
       ),
     );
